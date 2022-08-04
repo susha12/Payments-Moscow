346ee1001fb3e1960c77d18718d85ae288f35c99
susha <87581584+susha12@users.noreply.github.com>
2022-08-04 11:19:16 +0400

    changes one
    changes one

---
+++ assets/js/calculation.js
@@ -0,0 +1,142 @@
+
+
+
+
+
+
+// тут будет логика конвертации валюты и добавлении элементов
+const render = (tag, properties = null, children = null) => {
+    const element = document.createElement(tag);
+  
+    if (properties !== null) {
+      for (let [key, value] of Object.entries(properties)) {
+        if (key in element) {
+          element[key] = value;
+        } else {
+          element.setAttribute(key, value);
+        }
+      }
+    }
+    if (children !== null) {
+      if (children instanceof Array) {
+        element.append(...children);
+      } else {
+        element.append(children);
+      }
+    }
+    return element;
+  };
+  
+  // находим value списака предлогаемых банков и вкладываем в переменную
+  const banksListTransfersMoney = document.getElementById("banks_list_from_where_we_transfer_money");
+  
+  // находим списак предлогаемых банков и вкладываем в переменную
+  const banksTransfersMoney = document.getElementsByClassName("banks_from_where_we_transfer_money");
+  
+  // находим секцыю где отоброжаются те банки с которых можно перевести денги
+  let returnFinalValue = document.getElementById("returnFinalValue");
+  
+  // находим input где записывается входяшие данные 
+  let includesMoney = document.getElementById("includesMoney");
+  
+    // очистить список предложенных банков
+    // returnFinalValue.innerHTML = "";
+  
+  // при клике на входяший инпут его value исчезает
+  includesMoney.onclick=()=>{includesMoney.value = ""};
+  
+  // при клике на входяший инпут запускается функцыя которая подставляет значения исходяших банков а также комисионные и валюту
+  let arrayVariable = "01";
+  includesMoney.addEventListener("input", PlayСonversion);
+  
+    
+  // массывы которые показаны при выборе
+  const GroupOne = ['Наличные AED Дубаи','Наличные USD Дубаи','Любой банк AED','Юрлицо AED','Юрлицо USD'];
+  
+  const GroupTwo = ['Тинькофф RUB','Альфабанк RUB','Сбербанк RUB','МИР RUB','Любой банк RUB','Юрлицо RUB'];
+  
+  const GroupThree = ['Наличные AED Дубаи','Наличные USD Дубаи','Любой банк AED'];
+  
+  const GroupFour = ['Юрлицо AED','Юрлицо USD'];
+
+  // переменные выводяшего списка
+  let returnFinalOne = document.getElementById("returnFinalOne");
+  let returnFinalTwo = document.getElementById("returnFinalTwo");
+  let returnFinalThree = document.getElementById("returnFinalThree");
+  let returnFinalFour = document.getElementById("returnFinalFour");
+  let returnFinalFive = document.getElementById("returnFinalFive");
+  let returnFinalSix = document.getElementById("returnFinalSix");
+  
+  returnFinalSix.style.display = "none";
+  returnFinalValue.style.border = "2px solid red";
+  returnFinalValue.style.marginTop = "50px";
+  returnFinalValue.style.display = "flex";
+  function PlayСonversion() {
+  console.log("yes")
+    // определяем на каком масиве будем работать
+    if(banksListTransfersMoney.value == 1){
+      returnFinalOne.innerHTML = GroupOne[0];
+      returnFinalTwo.innerHTML = GroupOne[1];
+      returnFinalThree.innerHTML = GroupOne[2];
+      returnFinalFour.innerHTML = GroupOne[3];
+      returnFinalFive.innerHTML = GroupOne[4];
+      returnFinalSix.style.display = "none";
+      console.log("01")
+    //   returnFinalSix.remove()
+    }
+    else if(banksListTransfersMoney.value == 2){
+      returnFinalOne.innerHTML = GroupTwo[0];
+      returnFinalTwo.innerHTML = GroupTwo[1];
+      returnFinalThree.innerHTML = GroupTwo[2];
+      returnFinalFour.innerHTML = GroupTwo[3];
+      returnFinalFive.innerHTML = GroupTwo[4];
+      returnFinalSix.innerHTML = GroupTwo[5];
+      returnFinalSix.style.display = "flex";
+      console.log("02")
+    }
+    else if(banksListTransfersMoney.value == 3){
+      returnFinalOne.innerHTML = GroupThree[0];
+      returnFinalTwo.innerHTML = GroupThree[1];
+      returnFinalThree.innerHTML = GroupThree[2];
+      returnFinalFour.style.display = "none";
+      returnFinalFive.style.display = "none";
+      returnFinalSix.style.display = "none";
+      console.log("03")
+    //   returnFinalOne.innerHTML = GroupFour[0];
+    //   returnFinalTwo.innerHTML = GroupFour[1];
+    //   returnFinalThree.remove()
+    //   returnFinalFour.remove()
+    //   returnFinalFive.remove()
+    //   returnFinalSix.remove()
+    }
+    else if(banksListTransfersMoney.value == 4){
+      returnFinalOne.innerHTML = GroupFour[0];
+      returnFinalTwo.innerHTML = GroupFour[1];
+      returnFinalThree.style.display = "none";
+      returnFinalFour.style.display = "none";
+      returnFinalFive.style.display = "none";
+      returnFinalSix.style.display = "none";
+      console.log("04")
+    //   returnFinalOne.innerHTML = GroupFour[0];
+    //   returnFinalTwo.innerHTML = GroupFour[1];
+    //   returnFinalThree.remove()
+    //   returnFinalFour.remove()
+    //   returnFinalFive.remove()
+    //   returnFinalSix.remove()
+    }
+  }
+  
+
+  
+  
+  
+  
+
+  // box комиссия
+  let commission = document.getElementById("commission");
+  
+  // box курс конверсии
+  let conversionКate = document.getElementById("conversionКate");
+  
+  // input где показан расчет конвертации
+  let goingOutMoney = document.getElementById("goingOutMoney");
